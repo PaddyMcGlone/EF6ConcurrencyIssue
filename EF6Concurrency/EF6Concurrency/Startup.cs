9320ef5edef43aec013681f8e4b0a468c6164993
paddymcglone <paddymcglone@gmail.com>
2018-06-25 19:12:55 +0100

    The initial project commit - adding the bare bones.

---
+++ EF6Concurrency/EF6Concurrency/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EF6Concurrency.Startup))]
+namespace EF6Concurrency
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
